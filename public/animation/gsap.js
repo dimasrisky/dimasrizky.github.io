f1e5b3db09df49e8fa57cbac639dd9b449c9b75d
Kishiku_Joy17126 <rizkyalphazee@gmail.com>
2022-09-11 13:30:04 +0700

    menambahkan animasi on scroll pada about section

--- public/animation/gsap.js
+++ public/animation/gsap.js
@@ -8,3 +8,6 @@ gsap.from('#hero h2', {duration: 1.6, opacity: 0, x: '20%', ease: "power4", dela
 gsap.from('#hero p', {duration: 1.6, opacity: 0, x: '-20%', ease: "power4", delay: 1})
 gsap.from('img#gambar1', {duration: 1.4, opacity: 0,x: '25%', delay: 0.4,})
 gsap.from('#btn-hero', {duration: 1.1, opacity: 0,x: '-25%', delay: 1.2})
+
+
+
